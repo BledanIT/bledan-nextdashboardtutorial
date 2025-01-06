828baff628c9008849b57acc03061c51389e379e
BledanIT <bledan@gmail.com>
2025-01-06 13:50:17 +0100

    fix: delete button's error, attempt #2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,25 +7,6 @@ import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
-}
-
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -72,7 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   }
-  catch(err){
+  catch(error){
     return { message: 'Database Error: Failed to create invoice.'};
   }
   revalidatePath('/dashboard/invoices');
@@ -103,7 +84,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       WHERE id = ${id}
     `;
   }
-  catch(err){
+  catch(error){
     return { message: 'Database Error: Failed to Update Invoice.'};
   }
   revalidatePath('/dashboard/invoices');
@@ -111,11 +92,25 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (err) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
